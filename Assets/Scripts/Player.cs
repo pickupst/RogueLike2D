ef70052c9501deb1f41d24d039c7b3b48fa4fb2c
pickupst <gd.okanuslu@gmail.com>
2019-02-24 17:23:34 +0300

    Edit for mobile platforms

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,7 @@ public class Player : MovingObject
     private int pointsPerSoda = 50;
     private int pointsPerFood = 30;
 
+    private Vector2 touchOrigin = -Vector2.one;
     //-------------------------
 
     public int wallDamage = 1;
@@ -55,9 +56,46 @@ public class Player : MovingObject
         int horizontal = 0;
         int vertical = 0;
 
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
+
         horizontal = (int)Input.GetAxisRaw("Horizontal");
         vertical = (int)Input.GetAxisRaw("Vertical");
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
+        else if (vertical != 0)
+        {
+            horizontal = 0;
+        }
 
+#else
+        if (Input.touchCount > 0)
+        {
+            Touch myTouch = Input.touches[0];
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                touchOrigin = myTouch.position;
+            }
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = myTouch.position;
+
+                float x = touchEnd.x - touchOrigin.x;
+                float y = touchEnd.y - touchOrigin.y;
+
+                touchOrigin.x = -1;
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    horizontal = x > 0 ? 1 : -1;
+                }
+                else
+                {
+                    vertical = y > 0 ? 1 : -1;
+                }
+            }
+        }
+#endif
         if (horizontal != 0 || vertical != 0)
         {
             AttemptMove <Wall> (horizontal, vertical);
