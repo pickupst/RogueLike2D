4335389eb72195d30594d31e789941100cb0afe2
pickupst <gd.okanuslu@gmail.com>
2019-02-24 13:45:30 +0300

    Player hit and crrash wall

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,36 +4,57 @@ using UnityEngine;
 
 public abstract class MovingObject : MonoBehaviour
 {
+
+    public LayerMask blockingLayer;
+
     private BoxCollider2D boxCollider;
     private Rigidbody2D rb2D;
 
-    private void Awake()
+    protected virtual void Awake()
     {
         boxCollider = GetComponent<BoxCollider2D>();
         rb2D = GetComponent<Rigidbody2D>();
     }
 
-    protected virtual void AttemptMove(int horizontal, int vertical)
+    protected virtual void AttemptMove <T> (int horizontal, int vertical)
+        where T: Component
     {
+        RaycastHit2D hit;
 
-        Move(horizontal, vertical);
+        bool canMove = Move(horizontal, vertical, out hit);
 
+        if (hit.transform == null)
+        {
+            return;
+        }
+
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        if (!canMove && hitComponent != null)
+        {
+            OnCantMove(hitComponent);
+        }
     }
 
-    void Move(int horizontal, int vertical)
+    protected abstract void OnCantMove <T> (T component)
+        where T: Component;
+
+    bool Move(int horizontal, int vertical, out RaycastHit2D hit)
     {
         Vector2 start = transform.position;
         Vector2 end = start + new Vector2(horizontal, vertical);
 
         boxCollider.enabled = false;
-        RaycastHit2D hit = Physics2D.Linecast(start, end);
+        hit = Physics2D.Linecast(start, end, blockingLayer);
         boxCollider.enabled = true;
 
         if (hit.transform == null)
         {
             StartCoroutine(SmoothMovement(end));
+            return true;
         }
 
+        return false;
     }
 
     IEnumerator SmoothMovement(Vector3 end)
@@ -56,7 +77,15 @@ public abstract class MovingObject : MonoBehaviour
 
 public class Player : MovingObject
 {
+    Animator animator;
 
+    public int wallDamage = 1;
+
+    protected override void Awake()
+    {
+        animator = GetComponent<Animator>();
+        base.Awake();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -82,14 +111,23 @@ public class Player : MovingObject
 
         if (horizontal != 0 || vertical != 0)
         {
-            AttemptMove(horizontal, vertical);
+            AttemptMove <Wall> (horizontal, vertical);
         }
 
     }
 
-    protected override void AttemptMove(int horizontal, int vertical)
+    protected override void AttemptMove <T> (int horizontal, int vertical)
     {
-        base.AttemptMove(horizontal, vertical);
+        base.AttemptMove <T> (horizontal, vertical);
         GameManager.instance.playerTurn = false;
+    }
+
+    protected override void OnCantMove <T> (T component)
+    {
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall(wallDamage);
+
+        animator.SetTrigger("PlayerChop");
+
     }
 }
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public int hp = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DamageWall (int loss)
+    {
+
+        hp -= loss;
+        if (hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+}
